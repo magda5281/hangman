e00157b256750be61f39451e91660e850b2a14c0
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-17 19:51:03 +0000

    Update state of clickedLetters with keyboard events

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import words from './wordList.json';
 import HangmanDrawing from './components/hangmanDrawing';
 import HangmanWord from './components/hangmanWord';
@@ -7,11 +7,34 @@ function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
-
-  const [clickedLetters, setClickedLetters] = useState([]);
+  const [clickedLetters, setClickedLetters] = useState<string[]>([]);
   const incorrectLetters = clickedLetters.filter(
     (letter) => !wordToGuess.includes(letter),
   );
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (clickedLetters.includes(letter)) return;
+      setClickedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [clickedLetters],
+  );
+
+  // hook up keyboard with hangman drawing
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key: string = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+    document.addEventListener('keypress', handler);
+    return () => {
+      document.removeEventListener('keypress', handler);
+    };
+  }, [clickedLetters]);
+
   return (
     <section className="hangman_main">
       <h2>Loose Win </h2>
