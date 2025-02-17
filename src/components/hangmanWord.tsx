23423f281de307425e623506b3e58ed210a4e74a
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-17 16:46:31 +0000

    Add HangmanWord component

--- src/components/hangmanWord.tsx
+++ src/components/hangmanWord.tsx
@@ -1,3 +1,30 @@
 export default function HangmanWord() {
-  return <div>HangmanWord</div>;
+  const word = 'test';
+  const guessedLetters = ['e'];
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25rem',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
+      }}
+    >
+      {word.split('').map((letter, index) => (
+        <span style={{ borderBottom: '.1em solid black' }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? 'visible'
+                : 'hidden',
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
