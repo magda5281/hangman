e93e7d6b409f7ea9f285d09bed4e0f6c1a607086
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-17 16:22:54 +0000

    Add wordlist and hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import words from './wordList.json';
+import HangmanDrawing from './components/hangmanDrawing';
+import HangmanWord from './components/hangmanWord';
+import Keyboard from './components/keyboard';
 function App() {
-  return <h1>Hi</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  console.log(wordToGuess);
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  return (
+    <section className="hangman_main">
+      <h2>Loose Win </h2>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </section>
+  );
 }
 
 export default App;
